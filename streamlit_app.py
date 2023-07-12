c2a34cf3628d625b8ed5872b70fb537bff322458
shaik-jafar6 <112137448+shaik-jafar6@users.noreply.github.com>
2023-07-12 10:59:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/'+'kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/'+'fruit_choice')
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
